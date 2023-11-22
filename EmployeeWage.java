e24b21c44a8b4703c2e084e156103bd33b34ade9
“Roshni7605” <roshnimali847@gmail.com>
2023-11-22 09:09:22 +0530

    Refactored: Calculating Daily Employee Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,12 +1,23 @@
 class EmployeeWage {
 	
-	// Creating UC1 branch
 	public static void main(String[] args) {
+
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+
+		int empHours = 0;
+		int empWage = 0;
+
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if(empCheck == IS_FULL_TIME)
+		if(empCheck == IS_FULL_TIME){
 			System.out.println("Employee is present");
-		else
+			empHours=8;
+		}
+		else{
 			System.out.println("Employee is absent");
+			empHours=0;
+		}
+		empWage = empHours*EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage: " + empWage);
 	}
 }
